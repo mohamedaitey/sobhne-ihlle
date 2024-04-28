09c5bef8898a5e2798a8fb4ccd7c4612a0dbf633
mohamedaitey <165957429+mohamedaitey@users.noreply.github.com>
2024-04-28 23:45:16 +0300

    Add files via upload

---
+++ js/mian.js
@@ -0,0 +1,93 @@
+
+
+var counter = 0;
+
+ function incrment1(){
+    counter++;
+    document.getElementById("boot").innerHTML =counter;
+    
+ }
+function decrment1(){
+    counter=0;
+    document.getElementById("boot").innerHTML =counter;
+}
+
+// var counter = 0;
+
+ function incrment2(){
+    counter++;
+
+    document.getElementById("jjj").innerHTML =counter;
+    
+ }
+function decrment2(){
+    counter=0;
+    document.getElementById("jjj").innerHTML =counter;
+    
+}
+// var counter = 0;
+
+ function incrment3(){
+    counter++;
+
+    document.getElementById("www").innerHTML =counter;
+    
+ }
+function decrment3(){
+    counter=0;
+    document.getElementById("www").innerHTML =counter;
+    
+}
+// var counter = 0;
+
+ function incrment4(){
+    counter++;
+
+    document.getElementById("eee").innerHTML =counter;
+    
+ }
+function decrment4(){
+    counter=0;
+    document.getElementById("eee").innerHTML =counter;
+    
+}
+// var counter = 0;
+
+ function incrment5(){
+    counter++;
+
+    document.getElementById("rrr").innerHTML =counter;
+    
+ }
+function decrment5(){
+    counter=0;
+    document.getElementById("rrr").innerHTML =counter;
+    
+}
+// var counter = 0;
+
+ function incrment6(){
+    counter++;
+
+    document.getElementById("ttt").innerHTML =counter;
+    
+ }
+function decrment6(){
+    counter=0;
+    document.getElementById("ttt").innerHTML =counter;
+    
+}
+// var counter = 0;
+
+ function incrment7(){
+    counter++;
+
+    document.getElementById("yyy").innerHTML =counter;
+    
+ }
+function decrment7(){
+    counter=0;
+    document.getElementById("yyy").innerHTML =counter;
+    
+}
+
